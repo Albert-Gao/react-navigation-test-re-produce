9416a5b66a690f23b34cac50ba56802b8fbe5965
Albert Gao <albert.gao@lottonz.co.nz>
2020-07-18 16:24:58 +1200

    added setup.js for jest

--- App.test.tsx
+++ App.test.tsx
@@ -3,9 +3,6 @@ import App from "./App";
 import { render, act } from "react-native-testing-library";
 import { flushMicrotasksQueue } from "./testUtils";
 
-// Silence the warning https://github.com/facebook/react-native/issues/11094#issuecomment-263240420
-jest.mock("react-native/Libraries/Animated/src/NativeAnimatedHelper");
-
 it("should work", async () => {
   const tree = render(<App />);
 
